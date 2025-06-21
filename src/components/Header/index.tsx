7007c9e6c3fc6c0ccb1a87972cf24caab757edd9
arthurHisao <arthur.onaya@gmail.com>
2025-06-21 17:16:02 -0300

    Wip: Added sidebar effects

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,17 @@
 "use client"
 
-import { Menu } from 'lucide-react'
+import { Menu, Moon, Sun } from 'lucide-react'
 import { DropdownButton, DropdownContent, DropdownItem } from '../Dropdown'
+import { useState } from 'react'
 
 
 export default function Header({ onClick }: { onClick: React.MouseEventHandler<HTMLButtonElement>}) {
+  const [ isDarkTheme, setTheme ] = useState(false)
+
+  const handleToggleDark = () => {
+    setTheme(!isDarkTheme)
+  }
+
   return (
     <>
       <div className="flex min-h-16 border-b sticky top-0 bg-white px-4">
@@ -15,11 +22,31 @@ export default function Header({ onClick }: { onClick: React.MouseEventHandler<H
             </button>
           </div>
           <div className="inline-flex items-center gap-10">
-            <ul className="inline-flex gap-3">
-              <li>Página 1</li>
-              <li>Página 2</li>
-              <li>Página 3</li>
-            </ul>
+            <div className="inline-flex gap-3">
+              <label
+                htmlFor="change-mode"
+                className="block rounded-full w-12 h-7 relative cursor-pointer border border-gray-300 dark:border-white/15 bg-gray-200 text-gray-700 dark:bg-slate-700 dark:text-white"
+              >
+                <span
+                  className={`
+                    block relative w-6 h-6 top-[1px] bottom-0 left-0 right-0 rounded-full ease duration-300 cursor-pointer bg-white
+                    ${isDarkTheme ? 'translate-x-[21px] bg-slate-900' : 'translate-x-px'}
+                  `}
+                >
+                  {isDarkTheme ? (
+                    <Moon className='absolute top-0 left-0 right-0 p-1 w-6 h-6 rounded-full' />
+                  ) : (
+                    <Sun className='absolute top-0 left-0 right-0 p-1 w-6 h-6 rounded-full'/>
+                  )}
+                </span>
+                <input
+                  type="checkbox"
+                  id="change-mode"
+                  className="opacity-0 w-0 h-0"
+                  onClick={handleToggleDark}
+                />
+              </label>
+            </div>
 
             <DropdownButton 
               label='User Name'
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -20,16 +20,22 @@ export default function Sidebar() {
         <>
             <aside className='w-full h-screen sticky top-0'>
                 <div className='border-r h-full'>
-                    <div className='h-16 w-full flex items-center border-b px-3'>
+                    <div className='h-16 w-full flex items-center border-b px-2.5'>
                         <Link
                             href="#"
-                            className={`text-xl ${isSidebarCollapsed ? 'whitespace-nowrap overflow-hidden' : ''} w-full`}
+                            className='text-xl w-full whitespace-nowrap'
                         >
                             <div className='inline-flex items-center'>
-                                <span className="w-9 h-9 bg-amber-300 rounded-full inline-flex items-center justify-center shadow text-sm mr-2 font-bold">
+                                <span className="w-[43px] h-[43px] bg-amber-300 rounded-full inline-flex items-center justify-center shadow text-sm font-bold">
                                     { getInitials('Your Logo') }
                                 </span>
-                                <span className={`${isSidebarCollapsed ? 'opacity-0 invisible collapse-sidebar-text-2' : 'opacity-100 visible collapse-sidebar-text'}`}>
+                                <span className={`
+                                    ${isSidebarCollapsed 
+                                        ? 'opacity-0 invisible collapse-sidebar-text-2' 
+                                        : 'opacity-100 visible collapse-sidebar-text ml-2'
+                                    }
+                                    overflow-hidden
+                                `}>
                                     Your Logo
                                 </span>
                             </div>
@@ -39,40 +45,55 @@ export default function Sidebar() {
                     <nav className='h-full w-full py-4 px-2.5'>
                         <ul className='inline-flex flex-col justify-start w-full h-full divide-y'>
                             <li className='py-1.5 rounded'>
-                                <Link href="#" className='flex items-center gap-2 px-3'>
-                                    <div className="inline-flex items-center gap-2">
+                                <Link href="#" className='flex items-center px-3'>
+                                    <div className="inline-flex items-center">
                                         <LayoutDashboard className='h-4 w-4'/>
-                                        <span className={`${isSidebarCollapsed ? 'opacity-0 invisible collapse-sidebar-text-2' : 'opacity-100 visible collapse-sidebar-text'}`}>
+                                        <span className={`
+                                            ${isSidebarCollapsed 
+                                                ? 'opacity-0 invisible collapse-sidebar-text-2' 
+                                                : 'opacity-100 visible collapse-sidebar-text ml-2'
+                                            }
+                                        `}>
                                             Dashboard
                                         </span>
                                     </div>
                                 </Link>
                             </li>
                             <li className='py-1.5 rounded'>
-                                <Link href="#" className='flex items-center gap-2 px-3'>
-                                    <div className="inline-flex items-center gap-2">
+                                <Link href="#" className='flex items-center px-3'>
+                                    <div className="inline-flex items-center">
                                         <CircleSmall className='h-4 w-4' />                                        
-                                        <span className={`${isSidebarCollapsed ? 'opacity-0 invisible collapse-sidebar-text-2' : 'opacity-100 visible collapse-sidebar-text'}`}>
+                                         <span className={`
+                                            ${isSidebarCollapsed 
+                                                ? 'opacity-0 invisible collapse-sidebar-text-2' 
+                                                : 'opacity-100 visible collapse-sidebar-text ml-2'
+                                            }
+                                        `}>
                                             Product
                                         </span>
                                     </div>
                                 </Link>
                             </li>         
                             <li className='py-1.5 rounded'>
-                                <Link href="#" className='flex items-center gap-2 px-3'>
-                                <div className="inline-flex items-center gap-2">
-                                    <CircleSmall className='h-4 w-4' />
-                                    <span className={`${isSidebarCollapsed ? 'opacity-0 invisible collapse-sidebar-text-2' : 'opacity-100 visible collapse-sidebar-text'}`}>
-                                        Users
-                                    </span>
-                                </div>
+                                <Link href="#" className='flex items-center px-3'>
+                                    <div className="inline-flex items-center">
+                                        <CircleSmall className='h-4 w-4' />
+                                        <span className={`
+                                            ${isSidebarCollapsed 
+                                                ? 'opacity-0 invisible collapse-sidebar-text-2' 
+                                                : 'opacity-100 visible collapse-sidebar-text ml-2'
+                                            }
+                                        `}>
+                                            Users
+                                        </span>
+                                    </div>
                                 </Link>
                             </li>
-                            <li className='py-1.5 rounded'>
+                            {/* <li className='py-1.5 rounded'>
                                 <Accordion label='Permission' className="bg-indigo-500 rounded text-white font-medium">
                                     <p>Content should be rendered</p>
                                 </Accordion>
-                            </li>                           
+                            </li>                            */}
                         </ul>
                     </nav>
                 </div>
