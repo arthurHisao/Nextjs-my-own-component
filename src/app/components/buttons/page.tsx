fac7a60304b23073964ecd4e1fbb3b79e9e862df
arthurHisao <arthur.onaya@gmail.com>
2025-10-05 17:59:12 -0300

    Feat: update Button component to accept label prop and enhance button examples in the page

--- src/app/components/buttons/page.tsx
+++ src/app/components/buttons/page.tsx
@@ -3,15 +3,24 @@ import Button from '@/components/Button'
 export default function Page() {
     return (
         <div className="w-full px-4 flex flex-col h-[calc(100vh-112px)] overflow-auto">
-            <h2 className='mt-4 mb-1'>Buttons example</h2>
-            <div className='grid grid-rows-[2.5rem_100px] h-auto border rounded-t-sm divide-y items-center'>
-                <div className='px-4 py-1'>The button component has different size and color variations.</div>
-                <div className='px-4 py-3 flex gap-2 items-center'>
-                    <Button size='xs'/>
-                    <Button size='sm'/>
-                    <Button size='md' />
-                    <Button size='lg' />
-                    <Button size='xl' />
+            <h2 className='mt-4 mb-1.5 text-2xl'>Buttons example:</h2>
+
+            <div className='grid grid-flow-col grid-cols-2 grid-rows-[auto_auto] flex-wrap border rounded-t-sm  items-center'>
+                <div className='col-span-full px-4 h-full py-1.5 border-b'>The button component has different size and color variations.</div>
+                <div className='px-4 py-3 flex gap-2 items-center flex-wrap'>
+                    <Button label="Primary" color="primary" size='xs'/>
+                    <Button label="Primary" color="primary" size='sm'/>
+                    <Button label="Primary" color="primary" size='md' />
+                    <Button label="Primary" color="primary" size='lg' />
+                    <Button label="Primary" color="primary" size='xl' />
+                </div>                              
+                
+                <div className='px-4 py-3 flex gap-2 items-center flex-wrap'>
+                    <Button label="Default" size='xs'/>
+                    <Button label="Default" size='sm'/>
+                    <Button label="Default" size='md' />
+                    <Button label="Default" size='lg' />
+                    <Button label="Default" size='xl' />
                 </div>                
             </div>
         </div>
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,10 @@
 interface Properties {
     size?: string
     color?: string
+    label: string
 }
 
-export default function Button({ size, color }: Properties) {    
+export default function Button({ size, color, label }: Properties) {    
     const sizes = {
         'xs': '0', 
         'sm': '0.5', 
@@ -11,14 +12,22 @@ export default function Button({ size, color }: Properties) {
         'lg': '1.5', 
         'xl': '2'
     }
+
+    const colors = {
+        primary: 'bg-blue-500 hover:bg-blue-600/95 hover:border-blue-600 border border-blue-500 focus:border-blue-600 focus:bg-blue-600/95 ring-blue-500/50 shadow-blue-500/90',
+        positive: '',
+        negative: '',
+        warning: '',
+        info: ''
+    }
     
     return (
         <button className={`
             ${sizes ? `py-${sizes[size as keyof typeof size]}` : 'py-0.5'}
-            bg-blue-500 rounded-[3px] px-2.5 ease-in-out duration-300 hover:bg-blue-600/95 cursor-pointer 
-            border border-blue-500 focus:bg-blue-600/95 focus:ring-[.16rem] ring-blue-500/50 focus:shadow-inset shadow-blue-500/90 
+            ${colors[color as keyof typeof color] ?? 'bg-gray-400 hover:bg-gray-500/95 hover:border-gray-500 border border-gray-400 focus:border-gray-500 focus:bg-gray-500/95 ring-gray-500/50 shadow-gray-500/90'} 
+            cursor-pointer rounded-[3px] px-2.5 ease-in-out duration-300 focus:ring-[.16rem] focus:shadow-inset
         `}>
-            <span className="text-sm font-light tracking-tight text-white">Primary</span>
+            <span className="text-sm font-light tracking-tight text-white">{label}</span>
         </button>
     )
 }
