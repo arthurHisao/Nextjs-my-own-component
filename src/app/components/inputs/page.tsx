ea50bd87f84f391e97c02675f61afd62becf0133
arthurHisao <arthur.onaya@gmail.com>
2025-11-02 19:02:13 -0300

    feat: enhance Input component with size variations and update examples in Inputs page

--- src/app/components/inputs/page.tsx
+++ src/app/components/inputs/page.tsx
@@ -6,11 +6,22 @@ export default function Page() {
             <h2 className='mt-4 mb-1.5 text-2xl'>Inputs example:</h2>
 
             <div className='grid grid-rows-[auto] flex-wrap items-center dark:bg-[#19222e] rounded-t-xs border dark:border-[#19222e] dark:shadow-lg/35 shadow-[#19222e]'>
+                <div className='col-span-full px-4 h-full py-1.5 rounded-t-xs border-b'>The Input component has 4 different sizes:  xs, sm, lg, xl.</div>
+                <div className='px-4 py-3 flex gap-2 items-center flex-wrap'>
+                    <Input size="xs" placeholder="Type Something..."/>
+                    <Input size="sm" placeholder="Type Something" />
+                    <Input size="lg" placeholder="Type Something" />
+                    <Input size="xl" placeholder="Type Something" />
+                </div>                              
+            </div>
+
+            <div className='grid grid-rows-[auto] flex-wrap items-center dark:bg-[#19222e] rounded-t-xs border dark:border-[#19222e] dark:shadow-lg/35 shadow-[#19222e] mt-4'>
                 <div className='col-span-full px-4 h-full py-1.5 rounded-t-xs border-b'>The Input component has different variations.</div>
                 <div className='px-4 py-3 flex gap-2 items-center flex-wrap'>
                     <Input placeholder="Type Something..."/>
                     <Input placeholder="Type Password" type="password"/>
-                    <Input placeholder="Type Number" type="number"/>
+                    <Input placeholder="Type your email" type="email"/>
+                    <Input placeholder="Select file" type="file"/>
                 </div>                              
             </div>
         </div>
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,15 +2,66 @@ import { useId } from "react"
 
 interface Input {
   type?: string
+  size?: string
   placeholder?: string
 }
 
-function Input({ type, placeholder }: Input) {
+interface InputTypes {
+  [key: string]: string
+}
+
+function Input({ size = 'sm', type = 'text', placeholder }: Input) {
   const componentId = useId()
+  
+  const InputTypes: InputTypes = {
+    text: 'text',
+    password: 'password',
+    email: 'email',
+    file: 'file'
+  }
+
+  const sizes = {
+    xs: '0', 
+    sm: '0.5', 
+    md: '1', 
+    lg: '1.5', 
+    xl: '2'
+  }
+
   return (
     <div className="relative">
-      <input type={type} id={componentId} className="peer border rounded-xs px-1 py-0.5 outline-0 focus:ring-2 ring-blue-500/40 transition-all focus:border-blue-500" required/>
-      <label htmlFor={componentId} className="absolute top-0 left-0 font-extralight text-[12px] p-1.5 w-full h-full select-none peer-valid:hidden peer-placeholder-shown:hidden">{placeholder}</label>
+      { InputTypes[type] === 'file' ? (
+        <>
+          {/* <label 
+            htmlFor={componentId}
+            className={`
+              ${sizes ? `py-${sizes[size as keyof typeof sizes]}` : 'py-0.5'}
+              peer border rounded-xs px-1 outline-0 focus:ring-2 ring-blue-500/40 transition-all focus:border-blue-500 placeholder:text-xs block
+            `}
+          >
+            { placeholder }
+          </label> */}
+          <input
+            type={InputTypes[type]} 
+            id={componentId} 
+            className={`
+              ${sizes ? `py-${sizes[size as keyof typeof sizes]}` : 'py-0.5'}
+              peer border rounded-xs px-1 outline-0 focus:ring-2 ring-blue-500/40 transition-all focus:border-blue-500 placeholder:text-xs block
+              file:hidden
+            `}          
+          />
+        </>
+      ): (
+        <input
+          type={InputTypes[type]} 
+          id={componentId} 
+          className={`
+            ${sizes ? `py-${sizes[size as keyof typeof sizes]}` : 'py-0.5'}
+            peer border rounded-xs px-1 outline-0 focus:ring-2 ring-blue-500/40 transition-all focus:border-blue-500 placeholder:text-xs
+          `}
+          placeholder={placeholder}
+        />
+      )}
     </div>
   )
 }
