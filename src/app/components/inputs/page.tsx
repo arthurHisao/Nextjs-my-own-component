db8947cd3e1260562a246e8c0ea433913a1de137
arthurHisao <arthur.onaya@gmail.com>
2026-01-02 17:57:11 -0300

    enhaced input outlined and added sizes

--- src/app/components/inputs/page.tsx
+++ src/app/components/inputs/page.tsx
@@ -9,6 +9,7 @@ export default function Page() {
                 <div className='col-span-full px-4 h-full py-1.5 rounded-t-xs border-b'>The Input component has 4 different sizes: sm, md, lg, xl.</div>
                 <div className='px-4 py-3 flex gap-2 items-center flex-wrap'>
                     <Input size="sm" placeholder="Type Something" />
+                    <Input size="md" placeholder="Type Something" />
                     <Input size="lg" placeholder="Type Something" />
                     <Input size="xl" placeholder="Type Something" />
                 </div>                              
@@ -17,19 +18,46 @@ export default function Page() {
             <div className='grid grid-rows-[auto] flex-wrap items-center dark:bg-[#19222e] rounded-t-xs border dark:border-[#19222e] dark:shadow-lg/35 shadow-[#19222e] mt-4'>
                 <div className='col-span-full px-4 h-full py-1.5 rounded-t-xs border-b'>The Input component has different types.</div>
                 <div className='px-4 py-3 flex gap-2 items-center flex-wrap'>
-                    <Input placeholder="Type Something..."/>
-                    <Input placeholder="Type Password" type="password"/>
-                    <Input placeholder="Type your email" type="email"/>
-                    <Input placeholder="Select file" type="file"/>
-                    <Input placeholder="Text Area" type="textarea"/>
+                    <Input size="md" placeholder="Type Something..."/>
+                    <Input size="md" placeholder="Type Password" type="password"/>
+                    <Input size="md" placeholder="Type your email" type="email"/>
+                    <Input size="md" placeholder="Select file" type="file"/>
+                    <Input size="md" placeholder="Text Area" type="textarea"/>
                 </div>                              
             </div>
 
             <div className='grid grid-rows-[auto] flex-wrap items-center dark:bg-[#19222e] rounded-t-xs border dark:border-[#19222e] dark:shadow-lg/35 shadow-[#19222e] mt-4'>
-                <div className='col-span-full px-4 h-full py-1.5 rounded-t-xs border-b'>The Input has different variants.</div>
+                <div className='col-span-full px-4 h-full py-1.5 rounded-t-xs border-b'>The Input Outlined has different types.</div>
                 <div className='px-4 py-3 flex gap-2 items-center flex-wrap'>
-                    <Input placeholder="Input outlined" variant='outlined'/>
-                </div>                              
+                    <Input placeholder="text" variant='outlined'/>
+                    <Input type="password" placeholder="password" variant='outlined'/>
+                    <Input type="email" placeholder="email" variant='outlined'/>
+                    <Input type="textarea" placeholder="textarea" variant='outlined'/>
+                </div>   
+
+                {/* MD */}
+                <div className='px-4 py-3 flex gap-2 items-center flex-wrap'>
+                    <Input size="md" placeholder="text" variant='outlined'/>
+                    <Input size="md" type="password" placeholder="password" variant='outlined'/>
+                    <Input size="md" type="email" placeholder="email" variant='outlined'/>
+                    <Input size="md" type="textarea" placeholder="textarea" variant='outlined'/>
+                </div>                
+                
+                {/* LG */}
+                <div className='px-4 py-3 flex gap-2 items-center flex-wrap'>
+                    <Input size="lg" placeholder="text" variant='outlined'/>
+                    <Input size="lg" type="password" placeholder="password" variant='outlined'/>
+                    <Input size="lg" type="email" placeholder="email" variant='outlined'/>
+                    <Input size="lg" type="textarea" placeholder="textarea" variant='outlined'/>
+                </div>               
+                
+                {/* XL */}
+                <div className='px-4 py-3 flex gap-2 items-center flex-wrap'>
+                    <Input size="xl" placeholder="text" variant='outlined'/>
+                    <Input size="xl" type="password" placeholder="password" variant='outlined'/>
+                    <Input size="xl" type="email" placeholder="email" variant='outlined'/>
+                    <Input size="xl" type="textarea" placeholder="textarea" variant='outlined'/>
+                </div>
             </div>
         </div>
     )
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -21,13 +21,16 @@ function Input({ variant, size = 'sm', type = 'text', placeholder }: Input) {
   const componentId = useId()
 
   const inputRef = useRef<HTMLInputElement>(null)
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
   const [isFocused, setIsFocused] = useState(false)
+  const [hasValue, setHasValue] = useState(false)
   
   const InputTypes: InputTypes = {
     text: 'text',
     password: 'password',
     email: 'email',
-    file: 'file'
+    file: 'file',
+    textarea: 'textarea'
   }
 
   const styles: Variant = {
@@ -36,10 +39,15 @@ function Input({ variant, size = 'sm', type = 'text', placeholder }: Input) {
   }
 
   const sizes = {
-    sm: '0.5', 
-    md: '1', 
-    lg: '1.5', 
-    xl: '2'
+    // sm: '0.5', 
+    // md: '1', 
+    // lg: '1.5', 
+    // xl: '2'
+
+    sm: '8', 
+    md: '9', 
+    lg: '10',
+    xl: '11' 
   }
 
   const handleFocus = () => {
@@ -50,15 +58,79 @@ function Input({ variant, size = 'sm', type = 'text', placeholder }: Input) {
     setIsFocused(false)
   }
 
- 
-  if (InputTypes[type] === 'file') {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setHasValue(e.target.value.length > 0)
+  }
+
+  const heightValue = sizes[size as keyof typeof sizes] ? `calc(0.3 * ${sizes[size as keyof typeof sizes]}rem)` : 'calc(2.375rem);'
+
+  // Styled Inputs with variants
+  if (variant) {
+    return (
+      <div className="relative">
+        {InputTypes[type] === 'textarea' ? (
+          <>
+            <textarea
+              id={componentId}
+              ref={textareaRef}
+              className={`
+                peer border rounded-xs px-2.5 outline-0 focus:ring-2 ring-blue-500/40 transition-all placeholder:text-xs block placeholder-transparent min-h-10 py-2.5
+                dark:focus:bg-[#19222e] focus:border-0 focus:outline-none select-none
+              `}
+              style={{ height: `calc(0.5 * ${sizes[size as keyof typeof sizes]}rem)`}}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              onChange={handleChange}
+              placeholder={placeholder}
+            />
+            <label 
+              className={`
+                absolute pointer-events-none transition-all select-none
+                ${(isFocused || hasValue) ? '-top-1.5 text-xs px-1 dark:bg-[#19222e] bg-white ml-1.5 ' : 'top-0 text-sm z-10 flex items-center h-full px-2'}
+              `}
+            >
+              {placeholder}
+            </label>
+          </>
+        ) : (
+          <>
+            <input
+              type={InputTypes[type]}
+              id={componentId}
+              ref={inputRef}
+              className={`
+                peer border rounded-xs px-2.5 outline-0 focus:ring-2 ring-blue-500/40 transition-all placeholder:text-xs block placeholder-transparent
+                dark:focus:bg-[#19222e] focus:border-0 focus:outline-none select-none
+              `}
+              style={{ height: heightValue }}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              onChange={handleChange}
+              placeholder={placeholder}
+            />
+            <label 
+              className={`
+                absolute pointer-events-none transition-all select-none
+                ${(isFocused || hasValue) ? '-top-1.5 text-xs px-1 dark:bg-[#19222e] bg-white ml-1.5 ' : 'top-0 text-sm z-10 flex items-center h-full px-2'}
+              `}
+            >
+              {placeholder}
+            </label>
+          </>
+        )}
+      </div>
+    )
+  }
+
+  // Default Input Style
+  if (!variant && InputTypes[type] === 'file') {
     return (
       <>
           <input
             type={InputTypes[type]} 
             id={componentId} 
             className={`
-              ${sizes ? `py-${sizes[size as keyof typeof sizes]}` : 'py-0.5'}
+              ${sizes ? `h-${sizes[size as keyof typeof sizes]}` : 'py-0.5'}
               peer border rounded-xs px-1 outline-0 focus:ring-2 ring-blue-500/40 transition-all focus:border-blue-500 placeholder:text-xs block
               file:hidden
             `}          
@@ -66,39 +138,21 @@ function Input({ variant, size = 'sm', type = 'text', placeholder }: Input) {
       </>
     )
   }
-
-  const [hasValue, setHasValue] = useState(false)
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setHasValue(e.target.value.length > 0)
-  }
-
-  const paddingValue = sizes[size as keyof typeof sizes] ? `calc(1.2 * ${sizes[size as keyof typeof sizes]}rem) 0.5rem` : '0.6rem 0.5rem'
-
-  if (variant) {
+  
+  if (InputTypes[type] === 'textarea') {
     return (
-      <div className="relative">
-        <input
-          type={InputTypes[type]}
-          id={componentId}
-          ref={inputRef}
-          className={`
-            peer border rounded-xs px-2.5 outline-0 focus:ring-2 ring-blue-500/40 transition-all placeholder:text-xs block placeholder-transparent
-            dark:focus:bg-[#19222e] focus:border-0 focus:outline-none select-none
-          `}
-          style={{ padding: paddingValue }}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          onChange={handleChange}
-          placeholder={placeholder}
-        />
-        <label className={`
-            absolute pointer-events-none transition-all select-none
-            ${(isFocused || hasValue) ? '-top-1.5 text-xs px-1 dark:bg-[#19222e] bg-white ml-1.5' : 'top-0 px-3 py-3 text-sm z-10'}
-        `}>
-          {placeholder}
-        </label>
-      </div>
+      <>
+          <textarea
+            id={componentId}
+            ref={textareaRef}
+            placeholder={placeholder}
+            className={`
+              ${sizes ? `h-${sizes[size as keyof typeof sizes]}` : 'py-0.5'}
+              peer border rounded-xs px-1 outline-0 focus:ring-2 ring-blue-500/40 transition-all focus:border-blue-500 placeholder:text-xs block
+              file:hidden
+            `}          
+          />
+      </>
     )
   }
 
@@ -108,7 +162,7 @@ function Input({ variant, size = 'sm', type = 'text', placeholder }: Input) {
         type={InputTypes[type]} 
         id={componentId} 
         className={`
-          ${sizes ? `py-${sizes[size as keyof typeof sizes]}` : 'py-0.5'}
+          ${sizes ? `h-${sizes[size as keyof typeof sizes]}` : 'py-0.5'}
           peer border rounded-xs px-1 outline-0 focus:ring-2 ring-blue-500/40 transition-all focus:border-blue-500 placeholder:text-xs
         `}
         placeholder={placeholder}
